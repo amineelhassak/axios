9cad17d27d11f33e54fbfc1e2db272a2369636b3
amineelhassak <amineatrache57@gmail.com>
2026-03-08 00:41:27 +0000

    update

--- src/component/posts/post.tsx
+++ src/component/posts/post.tsx
@@ -1,32 +1,38 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function Post() {
   // 1. State to store your posts
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
   // 2. Define the function properly inside the component
   const fetchPosts = async () => {
     try {
-      const  { data }  = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts",
+      );
       setPosts(data);
       console.log(data);
     } catch (error) {
       console.error("Error fetching posts:", error);
+      setError(error);
     }
   };
 
   useEffect(() => {
     fetchPosts();
   }, []);
-  
+
   return (
     <div>
       <h2>Posts</h2>
       <ul>
-        {posts.slice(0, 5).map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
+        {error == null ? (
+          posts.map((post) => <li key={post.id}>{post.title}</li>)
+        ) : (
+          <p>Error fetching posts: {error.message}</p>
+        )}
       </ul>
     </div>
   );
